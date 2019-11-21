479bba5d10227af94e731b23aa1abdec41199df1
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-11-21 11:15:58 -0600

    November 21 IO walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,7 +4,7 @@ public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
         //The value of pi is approximately 3.14.
-        System.out.format("The value of pi is approximately %.2f \n", pi);
+        System.out.format("The value of pi is approximately %.2f %n", pi);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -29,12 +29,19 @@ public class ConsoleExercises {
 //
 //        System.out.println("Sentence: " + userInput);
 
-        System.out.print("Enter length and width: ");
-        int userInput1 = scanner.nextInt();
-        int userInput2 = scanner.nextInt();
+        System.out.print("Enter length: ");
+        int userInput1 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter width: ");
+        int userInput2 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter height: ");
+        int userInput3 = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("length: " + userInput1);
-        System.out.println("width: " + userInput2);
+        int perimeter = (userInput1*2) + (userInput2*2);
+        int area = userInput1*userInput2;
+        int volume = userInput1*userInput2*userInput3;
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+        System.out.println("Volume: " + volume);
 
 
 
