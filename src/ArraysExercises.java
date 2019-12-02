bad84981b2dcb16cec39041d13e05b75bfc2d659
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-12-02 16:12:01 -0600

    December 2nd fixed exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,20 +5,21 @@ public class ArraysExercises {
 //        Problem 1
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(numbers);
-
-        String[] personArray = {"John", "Bob", "Phil"};
-        String newPerson = "Jack";
-        String[] addedArray = addPerson(personArray, newPerson);
-        System.out.println(Arrays.toString(addedArray));
+        Person per1 = new Person("John");
+        Person per2 = new Person("Bob");
+        Person per3 = new Person("Phil");
+        Person[] personArray = {per1,per2,per3};
+//        String[] personArray = {"John", "Bob", "Phil"};
+        Person newPerson = new Person("Jack");
+        Person[] addedArray = addPerson(personArray, newPerson);
+//        System.out.println(Arrays.toString(addedArray));
 
 
     }
 
-    private static String[] addPerson(String[] arr, String newPerson) {
-        String[] returnArr = new String[arr.length + 1];
-        for (int i = 0; i < arr.length; i++) {
-            returnArr[i] = arr[i];
-        }
+    private static Person[] addPerson(Person[] arr, Person newPerson) {
+        Person[] returnArr = new Person[arr.length + 1];
+        System.arraycopy(arr, 0, returnArr, 0, arr.length);
         returnArr[arr.length] = newPerson;
         return returnArr;
     }
