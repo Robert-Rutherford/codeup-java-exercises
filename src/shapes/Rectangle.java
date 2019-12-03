428b106765cd81c88fccf249bc49e929199de096
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-12-03 13:59:01 -0600

    December 3rd exercise walkthrough

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,57 +1,57 @@
 package shapes;
 
 //        Inheritance and polymorphism Exercise
-//public class Rectangle {
-//    private int length;
-//    private int width;
-//
-//    public Rectangle(int Length, int Width){
-//        length = Length;
-//        width = Width;
-//    }
-//
-//    public int getLength(){
-//        return length;
-//    }
-//
-//    public int getWidth(){
-//        return width;
-//    }
-//
-//    public int getArea(){
-//        return length*width;
-//    }
-//
-//    public int getPerimeter(){
-//        return (2*length)+(2*width);
-//    }
-//
-//}
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int Length, int Width){
+        length = Length;
+        width = Width;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getArea(){
+        return length*width;
+    }
+
+    public int getPerimeter(){
+        return (2*length)+(2*width);
+    }
+
+}
 
 //      Abstract Classes and Interfaces Exercise
 
-public class Rectangle extends Quadrilateral implements Measurable {
-    public Rectangle(int Width, int Length) {
-        super(Width, Length);
-    }
-
-    @Override
-    public void setLength(int Length) {
-
-    }
-
-    @Override
-    public void setWidth(int Width) {
-
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 0;
-    }
-
-    @Override
-    public double getArea() {
-        return 0;
-    }
-}
+//public class Rectangle extends Quadrilateral implements Measurable {
+//    public Rectangle(int Width, int Length) {
+//        super(Width, Length);
+//    }
+//
+//    @Override
+//    public void setLength(int Length) {
+//
+//    }
+//
+//    @Override
+//    public void setWidth(int Width) {
+//
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return 0;
+//    }
+//}
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,48 +1,48 @@
 package shapes;
 
 //        Inheritance and polymorphism Exercise
-//public class Square extends Rectangle {
-//    private int side;
-//        public Square(int Side){
-//            super(Side,Side);
-//            side = Side;
-//        }
-//
-//        public int getArea(){
-//            return 4*side;
-//        }
-//
-//        public int getPerimeter(){
-//            return side*side;
-//        }
-//}
+public class Square extends Rectangle {
+    private int side;
+        public Square(int Side){
+            super(Side,Side);
+            side = Side;
+        }
+
+        public int getArea(){
+            return 4*side;
+        }
+
+        public int getPerimeter(){
+            return side*side;
+        }
+}
 
 //      Abstract Classes and Interfaces Exercise
-public class Square extends Quadrilateral{
-
-
-    public Square(int Width, int Length) {
-        super(Width, Length);
-    }
-
-    @Override
-    public void setLength(int Length) {
-
-    }
-
-    @Override
-    public void setWidth(int Width) {
-
-    }
-
-
-    @Override
-    public double getPerimeter() {
-        return 0;
-    }
-
-    @Override
-    public double getArea() {
-        return 0;
-    }
-}
+//public class Square extends Quadrilateral{
+//
+//
+//    public Square(int Width, int Length) {
+//        super(Width, Length);
+//    }
+//
+//    @Override
+//    public void setLength(int Length) {
+//
+//    }
+//
+//    @Override
+//    public void setWidth(int Width) {
+//
+//    }
+//
+//
+//    @Override
+//    public double getPerimeter() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return 0;
+//    }
+//}
